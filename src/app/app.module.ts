0106ce1afbbe0803337cc27c02d0454912a45e61
Travis Briggs <audiodude@gmail.com>
2018-04-01 12:24:15 -0700

    Get rid of useless FieldComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {BettingComponent} from './betting/betting.component';
 import {CardComponent} from './card/card.component';
 import {CardsComponent} from './cards/cards.component';
 import {ControlsComponent} from './controls/controls.component';
-import {FieldComponent} from './field/field.component';
 import {PaytableComponent} from './paytable/paytable.component';
-import { BettingComponent } from './betting/betting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FieldComponent,
     PaytableComponent,
     CardsComponent,
     ControlsComponent,
