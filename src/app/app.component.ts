617fca2a9be185be287597baaad07b0cc7447cd1
Travis Briggs <audiodude@gmail.com>
2018-04-05 10:10:03 -0700

    Pre-load card images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {Card, Deck, determineHand, Hand} from './deck';
 
@@ -7,7 +7,7 @@ import {Card, Deck, determineHand, Hand} from './deck';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   selections: boolean[];
   cards: Card[];
   deck: Deck;
@@ -191,5 +191,69 @@ export class AppComponent {
     this.cards = this.deck.deal(5);
     this.gameOver = true;
     this.clearBet = true;
+  }
+
+  ngOnInit() {
+    const cardPngs = [
+      '10_of_clubs.png',
+      '10_of_diamonds.png',
+      '10_of_hearts.png',
+      '10_of_spades.png',
+      '2_of_clubs.png',
+      '2_of_diamonds.png',
+      '2_of_hearts.png',
+      '2_of_spades.png',
+      '3_of_clubs.png',
+      '3_of_diamonds.png',
+      '3_of_hearts.png',
+      '3_of_spades.png',
+      '4_of_clubs.png',
+      '4_of_diamonds.png',
+      '4_of_hearts.png',
+      '4_of_spades.png',
+      '5_of_clubs.png',
+      '5_of_diamonds.png',
+      '5_of_hearts.png',
+      '5_of_spades.png',
+      '6_of_clubs.png',
+      '6_of_diamonds.png',
+      '6_of_hearts.png',
+      '6_of_spades.png',
+      '7_of_clubs.png',
+      '7_of_diamonds.png',
+      '7_of_hearts.png',
+      '7_of_spades.png',
+      '8_of_clubs.png',
+      '8_of_diamonds.png',
+      '8_of_hearts.png',
+      '8_of_spades.png',
+      '9_of_clubs.png',
+      '9_of_diamonds.png',
+      '9_of_hearts.png',
+      '9_of_spades.png',
+      'ace_of_clubs.png',
+      'ace_of_diamonds.png',
+      'ace_of_hearts.png',
+      'ace_of_spades.png',
+      'back.jpg',
+      'jack_of_clubs.png',
+      'jack_of_diamonds.png',
+      'jack_of_hearts.png',
+      'jack_of_spades.png',
+      'king_of_clubs.png',
+      'king_of_diamonds.png',
+      'king_of_hearts.png',
+      'king_of_spades.png',
+      'queen_of_clubs.png',
+      'queen_of_diamonds.png',
+      'queen_of_hearts.png',
+      'queen_of_spades.png',
+    ];
+    const baseUrl = '../assets/img/cards/';
+
+    for (const png of cardPngs) {
+      const img = new Image();
+      img.src = baseUrl + png;
+    }
   }
 }
